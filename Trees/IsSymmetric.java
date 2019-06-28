31a8f428e85d244c467549239f7747c8b54f6596
Indra Alapati <indrababu1547@gmail.com>
2019-06-28 17:24:46 -0500

    Create IsSymmetric.java

---
+++ Trees/IsSymmetric.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ /*
+ Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ */
+ 
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+    public boolean isSymmetric(TreeNode left, TreeNode right){
+        if(left == null || right == null){
+            return left == right;
+        }
+        if(left.val != right.val){
+            return false;
+        }
+        return isSymmetric (left.left,right.right) && isSymmetric(left.right,right.left);
+    }
+}
