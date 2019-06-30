d27074a494c4681d75ad06ef0a527bc58de5bf7f
Indra Alapati <indrababu1547@gmail.com>
2019-06-30 15:27:43 -0500

    Update MutuallyExclusiveIntervals.java

--- Arrays/MutuallyExclusiveIntervals.java
+++ Arrays/MutuallyExclusiveIntervals.java
@@ -1,5 +1,15 @@
 package com.indra.CodingPatterns.MergeIntervals;
 
+/*
+Algorithm:
+1. Sort the intervals on the start time to ensure a.start <= b.start
+2. If ‘a’ overlaps ‘b’ (i.e. b.start <= a.end), we need to merge them into a new interval ‘c’ such that:
+    c.start = a.start
+    c.end = max(a.end, b.end)
+3. We will keep repeating the above two steps to merge ‘c’ with the next interval if it overlaps with ‘c’.
+
+*/
+
 import java.util.*;
 
 class Interval {
