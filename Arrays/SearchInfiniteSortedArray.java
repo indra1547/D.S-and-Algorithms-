0978faa980af23b17e5e83cc94f93d5101363867
Indra Alapati <indrababu1547@gmail.com>
2019-07-11 14:24:49 -0500

    Create SearchInfiniteSortedArray.java

---
+++ Arrays/SearchInfiniteSortedArray.java
@@ -0,0 +1,63 @@
+package com.indra.CodingPatterns.BinarySearch;
+/*
+ * Given an infinite sorted array (or an array with unknown size), find if a 
+ * given number ‘key’ is present in the array. Write a function to return the 
+ * index of the ‘key’ if it is present in the array, otherwise return -1.
+   Since it is not possible to define an array with infinite (unknown) size, 
+   you will be provided with an interface ArrayReader to read elements of the 
+   array. ArrayReader.get(index) will return the number at index; if the array
+   ’s size is smaller than the index, it will return Integer.MAX_VALUE.
+   
+   Input: [4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30], key = 16
+   Output: 6
+   Explanation: The key is present at index '6' in the array.
+ * */
+
+class ArrayReader {
+	int[] arr;
+
+	ArrayReader(int[] arr) {
+		this.arr = arr;
+	}
+
+	public int get(int index) {
+		if (index >= arr.length)
+			return Integer.MAX_VALUE;
+		return arr[index];
+	}
+}
+
+public class SearchInfiniteSortedArray {
+	public int search(ArrayReader reader, int key) {
+		// find proper bounds
+		int start = 0;
+		int end = 1;
+		while (reader.get(end) < key) {
+			int newStart = end + 1;
+			end = end +  (end - start + 1) * 2; // increase double the bound size
+			start = newStart;
+		}
+		return binarySearch(reader, start, end, key);
+	}
+
+	private int binarySearch(ArrayReader reader, int start, int end, int key) {
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (reader.get(mid) < key) {
+				start = mid + 1;
+			} else if (reader.get(mid) > key) {
+				end = mid - 1;
+			} else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		ArrayReader reader = new ArrayReader(new int[] { 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30 });
+		System.out.println(new SearchInfiniteSortedArray().search(reader, 16));
+		System.out.println(new SearchInfiniteSortedArray().search(reader, 11));
+	}
+
+}
